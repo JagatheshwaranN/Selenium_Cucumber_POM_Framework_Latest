116498e00d868b7f984a347151cec51c3275763b
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-03-15 14:32:03 +0530

    Ticket Book Page and Ticket Details Page Elements Updated, Dry Run Inprogress

--- src/test/java/com/jtaf/qa/pages/TicketBookingPage.java
+++ src/test/java/com/jtaf/qa/pages/TicketBookingPage.java
@@ -26,15 +26,17 @@ public class TicketBookingPage extends HomePage {
 
 	private static Logger log = LoggerUtility.getLog(TicketBookingPage.class);
 
-	@FindBy(how = How.XPATH, using = "//div[contains(@class,'NewSortOptionsstyles')]//div[@class='fb txtUpper dF alignItemsCenter']//span[text()='PRICE']")
+	@FindBy(how = How.XPATH, using = "//button[contains(@class,'button buttonSecondry buttonBig')]")
+	public WebElement ticketBookPopupClose;
+	@FindBy(how = How.XPATH, using = "//div[contains(@class,'price_sorter')]//span[@class='pointer']")
 	public WebElement priceSort;
-	@FindBy(how = How.XPATH, using = "(//div[contains(@class,'srp-card-uistyles__CardRight')]//button[contains(@class,'srp-card-uistyles__BookButton') and text()='VIEW FARES'])[1]")
-	public WebElement viewFares;
-	@FindBy(how = How.XPATH, using = "(//div[contains(@class,'srp-card-uistyles__Price-sc-3flq99-17')])[1]")
+	@FindBy(how = How.XPATH, using = "(//button[contains(@class,'ViewFareBtn')]//span[@class='appendRight8'])[1]")
+	public WebElement viewPrices;
+	@FindBy(how = How.XPATH, using = "(//div[@class='priceSection']//p)[1]")
 	public WebElement priceList;
-	@FindBy(how = How.XPATH, using = "(//input[contains(@class,'srp-card-uistyles__Fltbook-sc-3flq99-35')])[1]")
+	@FindBy(how = How.XPATH, using = "(//div[contains(@class,'viewFareBtnCol')]//button[contains(@id,'bookbutton')])[1]")
 	public WebElement book;
-	@FindBy(how = How.XPATH, using = "(//div[@class='dF alignItemsCenter']//span[contains(@class,'padL5 black')])[1]")
+	@FindBy(how = How.XPATH, using = "(//p[contains(@class,'airlineName')])[1]")
 	public WebElement flightName;
 
 	public TicketBookingPage(WebDriver driver) {
@@ -46,6 +48,10 @@ public class TicketBookingPage extends HomePage {
 		return getPageTitle();
 	}
 
+	public WebElement getTicketBookPopupClose() {
+		return ticketBookPopupClose;
+	}
+
 	public WebElement getPriceSort() {
 		return priceSort;
 	}
@@ -54,8 +60,8 @@ public class TicketBookingPage extends HomePage {
 		return priceList;
 	}
 
-	public WebElement getViewFares() {
-		return viewFares;
+	public WebElement getviewPrices() {
+		return viewPrices;
 	}
 
 	public WebElement getBook() {
@@ -68,11 +74,10 @@ public class TicketBookingPage extends HomePage {
 
 	public void verifyTicketBookingTitle() {
 		try {
+			reusableHelper.elementClick(getTicketBookPopupClose(), "ticketBookPopupClose");
 			browserHelper.getCurrentPageUrl();
 			Assert.assertEquals(getTicketBookingPageTitle(),
 					FileReaderUtility.getTestData("ticket.booking.page.title"));
-			//Trail and Error
-			Thread.sleep(120000);
 		} catch (Exception ex) {
 			log.info("Error occured while check ticket booking page title" + "\n" + ex);
 			Assert.fail();
@@ -84,14 +89,16 @@ public class TicketBookingPage extends HomePage {
 		int lowestPrice = 0;
 		try {
 			verificationHelper.verifyElementPresent(getPriceList(), "priceList");
-			price = Integer.parseInt(getPriceList().getText().toString().substring(0).replaceAll(",", ""));
+			price = Integer.parseInt(getPriceList().getText().toString().substring(1).replaceAll(",", "").trim());
 			lowestPrice = getLowestPrice();
 			if (price == lowestPrice) {
 				ReusableHelper.setAnyElement("flightName",
 						verificationHelper.readTextValueFromElement(getFlightName(), "flightName"));
-				reusableHelper.elementClick(getViewFares(), "viewFaresButton");
+				reusableHelper.elementClick(getviewPrices(), "viewPricesButton");
 				verificationHelper.verifyElementPresent(getBook(), "bookButton");
 				reusableHelper.elementClick(getBook(), "bookButton");
+				// Trail and Error
+				Thread.sleep(5000);
 			}
 		} catch (Exception ex) {
 			log.info("Error occured while book the ticket" + "\n" + ex);
@@ -105,11 +112,11 @@ public class TicketBookingPage extends HomePage {
 		String price = null;
 		int priceInNum = 0;
 		try {
-			String priceList = "(//div[contains(@class,'srp-card-uistyles__Price-sc-3flq99-17')])";
+			String priceList = "(//div[@class='priceSection']//p)";
 			int size = getDriver().findElements(By.xpath(priceList)).size();
 			for (int i = 1; i <= size; i++) {
 				price = getDriver().findElement(By.xpath(priceList + "[" + i + "]")).getText();
-				price = price.substring(0).replaceAll(",", "");
+				price = price.substring(1).replaceAll(",", "").trim();
 				priceInNum = Integer.parseInt(price);
 				priceBucket.add(priceInNum);
 			}
--- src/test/java/com/jtaf/qa/pages/TicketDetailsPage.java
+++ src/test/java/com/jtaf/qa/pages/TicketDetailsPage.java
@@ -20,19 +20,19 @@ public class TicketDetailsPage extends TicketBookingPage {
 
 	private static Logger log = LoggerUtility.getLog(TicketDetailsPage.class);
 
-	@FindBy(how = How.XPATH, using = "//span[contains(@class,'flex1') and text()='TICKET DETAILS']")
+	@FindBy(how = How.XPATH, using = "//h2[contains(@class,'headerTitle')]")
 	public WebElement ticketDetailsHeader;
-	@FindBy(how = How.XPATH, using = "//div[contains(@class,'common-elementsstyles__Wid13')]//span[1]")
+	@FindBy(how = How.XPATH, using = "//div[contains(@class,'flightItenaryHdr')]//p//span[1]")
 	public WebElement ticketDetailsFlightName;
-	@FindBy(how = How.XPATH, using = "//div[contains(@class,'common-elementsstyles__Wid13')]//span[2]")
+	@FindBy(how = How.XPATH, using = "//div[contains(@class,'flightItenaryHdr')]//div[@class='makeFlex']//span[1]")
 	public WebElement ticketDetailsTravelClass;
-	@FindBy(how = How.XPATH, using = "(//div[contains(@class,'common-elementsstyles__Wid31')]//span[contains(@class,'padR5')])[1]")
+	@FindBy(how = How.XPATH, using = "(//div[contains(@class,'itenaryLeft')]//div[contains(@class,'makeFlex')]//div//span)[3]")
 	public WebElement ticketDetailsFromPlace;
-	@FindBy(how = How.XPATH, using = "(//div[contains(@class,'common-elementsstyles__Wid31')]//span[contains(@class,'padR5')])[4]")
+	@FindBy(how = How.XPATH, using = "(//div[contains(@class,'itenaryLeft')]//div[contains(@class,'makeFlex')]//div//span)[7]")
 	public WebElement ticketDetailsToPlace;
-	@FindBy(how = How.ID, using = "fareSummary")
+	@FindBy(how = How.ID, using = "FARE_SUMMARY")
 	public WebElement ticketFareSummary;
-	@FindBy(how = How.XPATH, using = "//div[@class='price-breakupstyles__BreakupTitle-sc-fjdxc9-14 eovIVn fb go_blue']")
+	@FindBy(how = How.XPATH, using = "//p[@class='fareRow']//span[2]")
 	public WebElement ticketTotalAmount;
 
 	public TicketDetailsPage(WebDriver driver) {
